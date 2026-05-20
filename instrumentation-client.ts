f400f5c359989bebb5b0c9c32398336dc6bb54a6
Aiden Tabrizi <aidentabrizi@icloud.com>
2026-05-20 02:34:25 -0700

    Change ui host

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,7 +2,7 @@ import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: '/ingest',
-  ui_host: 'https://us.posthog.com',
+  ui_host: 'https://wsp.aidenkt.com',
   defaults: '2026-01-30',
   capture_exceptions: true,
   debug: process.env.NODE_ENV === 'development',
